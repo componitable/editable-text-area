04e919ec423f581b7fb9bc350643be877c7318e6
ForbesLindesay <forbes@lindesay.co.uk>
2013-01-17 14:12:39 +0000

    Release 0.0.2

--- index.js
+++ index.js
@@ -1,56 +1,77 @@
 var emitter = require('emitter');
 var editable = require('editable');
+var toArray = require('to-element-array');
+var matches = require('matches-selector');
 
 module.exports = makeEditable;
 function makeEditable(elements, options) {
-    options = options || {};
-    editable.click(elements, function (element) {
-        if (element.getAttribute('data-in-edit-mode') == 'true') return;
-        element.setAttribute('data-in-edit-mode', 'true');
-        edit(element, options);
-    });
+  options = options || {};
+  editable.click(elements, function (element) {
+    if (element.getAttribute('data-in-edit-mode') == 'true') return;
+    element.setAttribute('data-in-edit-mode', 'true');
+    edit(element, options);
+  });
+  setInterval(fixElements, 500);
+  fixElements();
+  function fixElements() {
+    var els = toArray(elements);
+    for (var i = 0; i < els.length; i++) {
+      if (els[i].getElementsByTagName('input').length === 0 && !els[i].hasAttribute('data-real-text')) {
+        fixElement(els[i], els[i].textContent);
+      }
+    }
+  }
 }
 emitter(makeEditable);
 
+function fixElement(el, content) {
+  var normalized = content.trim().replace(/\r?\n/g, '\n\n').replace(/\n\n\n+/g, '\n\n');
+  el.setAttribute('data-real-text', normalized);
+  el.innerHTML = '<p>' + normalized.replace(/\n\n/g, '</p><p>') + '</p>';
+  return normalized;
+}
+
+
+
 function edit(element, options) {
-    var dimensions;
-    var oldStyle;
+  var dimensions;
+  var oldStyle;
+  if (options.maintainSize === true) {
+    dimensions = editable.dimensions(element);
+  }
+  emit('pre-begin-edit', element);
+  var value = element.getAttribute('data-real-text') || fixElement(element, element.textContent);
+  element.innerHTML = '';
+  var edit = document.createElement('textarea');
+  edit.value = value;
+  element.appendChild(edit);
+  if (options.maintainSize === true) {
+    var editDimensions = editable.transformDimensions(edit, dimensions);
+    edit.style.width = editDimensions.width + 'px';
+    edit.style.height = editDimensions.height + 'px';
+    oldStyle = {width: element.style.width, height: element.style.height};
+    element.style.width = dimensions.width + 'px';
+    element.style.height = dimensions.height + 'px';
+  }
+  edit.focus();
+  editable.blur(edit, function () {
+    if (element.getAttribute('data-in-edit-mode') != 'true') return;
+    element.setAttribute('data-in-edit-mode', 'false');
+    emit('pre-end-edit', element);
+    var result = fixElement(element, edit.value.trim());
     if (options.maintainSize === true) {
-        dimensions = editable.dimensions(element);
+      element.style.width = oldStyle.width;
+      element.style.height = oldStyle.height;
     }
-    emit('pre-begin-edit', element);
-    var value = element.textContent.trim();
-    element.innerHTML = '';
-    var edit = document.createElement('textarea');
-    edit.value = value;
-    element.appendChild(edit);
-    if (options.maintainSize === true) {
-        var editDimensions = editable.transformDimensions(edit, dimensions);
-        edit.style.width = editDimensions.width + 'px';
-        edit.style.height = editDimensions.height + 'px';
-        oldStyle = {width: element.style.width, height: element.style.height};
-        element.style.width = dimensions.width + 'px';
-        element.style.height = dimensions.height + 'px';
+    if (value != result) {
+      emit('update', element, result);
     }
-    edit.focus();
-    editable.blur(edit, function () {
-        if (element.getAttribute('data-in-edit-mode') != 'true') return;
-        element.setAttribute('data-in-edit-mode', 'false');
-        emit('pre-end-edit', element);
-        element.innerHTML = edit.value.trim();
-        if (options.maintainSize === true) {
-            element.style.width = oldStyle.width;
-            element.style.height = oldStyle.height;
-        }
-        if (value != edit.value.trim()) {
-            emit('update', element, edit.value.trim());
-        }
-        emit('post-end-edit', element);
-    });
-    emit('post-begin-edit', element);
+    emit('post-end-edit', element);
+  });
+  emit('post-begin-edit', element);
 }
 
 function emit() {
-    module.exports.emit.apply(module.exports, arguments);
-    editable.emit.apply(editable, arguments);
+  module.exports.emit.apply(module.exports, arguments);
+  editable.emit.apply(editable, arguments);
 }
